35e5c1601672935575f394f5b2683750c38ca60d
Xudong Chen <cxd9206@gmail.com>
2019-02-10 15:08:58 -0500

    add function to preprocess the image for input of the network

--- preprocess.py
+++ preprocess.py
@@ -42,6 +42,20 @@ def prep_image(img, inp_dim):
     img_ = torch.from_numpy(img_).float().div(255.0).unsqueeze(0)
     return img_, orig_im, dim
 
+
+def prep_image_content(orig_im, inp_dim):
+    """
+    Prepare image for inputting to the neural network.
+
+    Returns a Variable
+    """
+    dim = orig_im.shape[1], orig_im.shape[0]
+    img = (letterbox_image(orig_im, (inp_dim, inp_dim)))
+    img_ = img[:, :, ::-1].transpose((2, 0, 1)).copy()
+    img_ = torch.from_numpy(img_).float().div(255.0).unsqueeze(0)
+    return img_, orig_im, dim
+
+
 def prep_image_pil(img, network_dim):
     orig_im = Image.open(img)
     img = orig_im.convert('RGB')
